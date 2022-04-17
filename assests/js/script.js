f356b9a71ea442d32112b2f95fc66f5566ebc459
Salma <salmaabdi7494@gmail.com>
2022-04-17 13:00:24 -0500

    allows for more functionality

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,3 +1,5 @@
+var taskIdCounter = 0; //generates a unique id
+
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
@@ -26,6 +28,40 @@ var createTaskEl = function (taskDataObj) {
   // create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
+
+  listItemEl.setAttribute("data-task-id", taskIdCounter++);
+
+var createTaskActions = function(taskId){
+  var actionContainerEl = document.createElement("div");
+  actionContainerEl.className = "task-actions";
+return actionContainerEl;
+}
+//return actionContainerEl;
+
+
+// create edit button
+var editButtonEl = document.createElement("button");
+editButtonEl.textContent = "Edit";
+editButtonEl.className = "btn edit-btn";
+editButtonEl.setAttribute("data-task-id", taskId);
+
+actionContainerEl.appendChild(editButtonEl);
+
+// create delete button
+var deleteButtonEl = document.createElement("button");
+deleteButtonEl.textContent = "Delete";
+deleteButtonEl.className = "btn delete-btn";
+deleteButtonEl.setAttribute("data-task-id", taskId);
+
+actionContainerEl.appendChild(deleteButtonEl);
+
+var statusSelectEl = document.createElement("select");
+statusSelectEl.className = "select-status";
+statusSelectEl.setAttribute("name", "status-change");
+statusSelectEl.setAttribute("data-task-id", taskId);
+
+actionContainerEl.appendChild(statusSelectEl);
+
 
   // create div to hold task info and add to list item
   var taskInfoEl = document.createElement("div");
